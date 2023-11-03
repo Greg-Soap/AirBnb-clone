d591591ad0753ee214faed59fd34496720b0b57d
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-11-03 06:30:31 +0100

    changes and addition of icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,39 @@
-import { Nunito } from 'next/font/google'
+import { Nunito } from "next/font/google";
 
-import Navbar from '@/app/components/navbar/Navbar';
-import LoginModal from '@/app/components/modals/LoginModal';
-import RegisterModal from '@/app/components/modals/RegisterModal';
-import SearchModal from '@/app/components/modals/SearchModal';
-import RentModal from '@/app/components/modals/RentModal';
+import Navbar from "@/app/components/navbar/Navbar";
+import LoginModal from "@/app/components/modals/LoginModal";
+import RegisterModal from "@/app/components/modals/RegisterModal";
+import SearchModal from "@/app/components/modals/SearchModal";
+import RentModal from "@/app/components/modals/RentModal";
 
-import ToasterProvider from '@/app/providers/ToasterProvider';
+import ToasterProvider from "@/app/providers/ToasterProvider";
 
-import './globals.css'
-import ClientOnly from './components/ClientOnly';
-import getCurrentUser from './actions/getCurrentUser';
+import "./globals.css";
+import ClientOnly from "./components/ClientOnly";
+import getCurrentUser from "./actions/getCurrentUser";
 
 export const metadata = {
-  title: 'Airbnb',
-  description: 'Airbnb Clone',
-}
+  title: "HomeStay Haven - Your Gateway to Affordable Living",
+  description:
+    "Explore budget-friendly leases and experience comfort without compromise at HomeStay Haven. Discover the perfect blend of quality and affordability.",
+  keywords:
+    "HomeStay Haven, affordable living, cheap leases, budget-friendly housing, comfortable homes",
+  ogTitle: "HomeStay Haven - Your Gateway to Affordable Living",
+  ogDescription:
+    "Explore budget-friendly leases and experience comfort without compromise at HomeStay Haven. Discover the perfect blend of quality and affordability.",
+  ogType: "website",
+  ogImage: "/images/favicon.ico", // Add the actual path to your website's image for better visual appeal
+  ogUrl: "https://www.homestayhaven.vercel.app", // Replace with your website URL
+};
 
-const font = Nunito({ 
-  subsets: ['latin'], 
+const font = Nunito({
+  subsets: ["latin"],
 });
 
 export default async function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   const currentUser = await getCurrentUser();
 
@@ -39,10 +48,8 @@ export default async function RootLayout({
           <RentModal />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        <div className="pb-20 pt-28">
-          {children}
-        </div>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
-  )
+  );
 }
