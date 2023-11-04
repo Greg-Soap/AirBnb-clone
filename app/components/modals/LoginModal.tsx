b4109a337d4d7bad702b8b92b410e71a85a31ba9
Osianic <56081021+Greg-Soap@users.noreply.github.com>
2023-11-04 16:39:35 +0100

    finally

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -102,7 +102,7 @@ const LoginModal = () => {
       text-neutral-500 text-center mt-4 font-light"
       >
         <p>
-          First time using Airbnb?
+          First time using HomeStay?
           <span
             onClick={onToggle}
             className="
--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -271,7 +271,7 @@ const RentModal = () => {
     <Modal
       disabled={isLoading}
       isOpen={rentModal.isOpen}
-      title="Airbnb your home!"
+      title="Lease your home!"
       actionLabel={actionLabel}
       onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
